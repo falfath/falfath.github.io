10e105127453ad56a8d3437259f75c26c89ceae4
Fahmi Alfath <fahmi243alfath@gmail.com>
2020-06-23 23:31:04 +0700

    update data

--- push.js
+++ push.js
@@ -1,4 +1,4 @@
-var webPush = require('web-push');
+const webPush = require('web-push');
  
 const vapidKeys = {
    "publicKey": "BDc1rx4F_Gd2anygj7RitO2_MrQPeggmR9305YQOuKrT8kJ0yIqAuJ2oLbSyyu4E1bVmqxQCn7ifqc7ACR40ZZM",
@@ -11,16 +11,16 @@ webPush.setVapidDetails(
    vapidKeys.publicKey,
    vapidKeys.privateKey
 )
-var pushSubscription = {
+const pushSubscription = {
    "endpoint": "https://fcm.googleapis.com/fcm/send/dxsUZvXttgw:APA91bFVkFzAOWiUonpruX0vW0hej3y7mYGpzPDYiBf9pv2fNHktZtsS08SAKfBDLvOWQUxG3o_TN5UAHHMlksIWvrGBRoEwLI1D_dmPYW8y6StBhT0to1JMwi3kXHlOQoKhGiHhQai2",
    "keys": {
        "p256dh": "BMDuTbue5g5nkA0OZDpkKcylAU4fwunS050i7ERgHBXJyn7Teq5PKu7bBzZY+ykoxC4CLhk7guN5ujC+DxMe3L8=",
        "auth": "H7icgQywy4A6aj0hxNrguA=="
    }
 };
-var payload = 'Congratulations! This app now can use push notifications!';
+let payload = 'Congratulations! This app now can use push notifications!';
  
-var options = {
+let options = {
    gcmAPIKey: '1013511244177',
    TTL: 60
 };
--- service-worker.js
+++ service-worker.js
@@ -39,107 +39,26 @@ workbox.routing.registerRoute(
 	  workbox.strategies.staleWhileRevalidate({
 		  cacheName: 'bundesliga-app-v2'
 	  })
-  );
+);
 
-// const CACHE_NAME 	= "bundesliga-app-v1";
-// var urlsToCache		= [
-// 	"/",
-// 	"/index.html",
-// 	"/team-detail.html",
-// 	"/nav.html",
-// 	"/pages/about.html",
-// 	"/pages/favorites.html",
-// 	"/pages/home.html",
-// 	"/pages/matches.html",
-// 	"/pages/standings.html",
-// 	"/pages/teams.html",
-// 	"/assets/css/custom.css",
-// 	"/assets/css/materialize.min.css",
-// 	"/assets/js/api.js",
-// 	"/assets/js/db.js",
-// 	"/assets/js/detail.js",
-// 	"/assets/js/helper.js",
-// 	"/assets/js/idb.js",
-// 	"/assets/js/main.js",
-// 	"/assets/js/materialize.min.js",
-// 	"/assets/images/about-me.jpg",
-// 	"/assets/images/icons/favicon.ico",
-// 	"/assets/images/icons/icon-72x72.png",
-// 	"/assets/images/icons/icon-96x96.png",
-// 	"/assets/images/icons/icon-128x128.png",
-// 	"/assets/images/icons/icon-144x144.png",
-// 	"/assets/images/icons/icon-152x152.png",
-// 	"/assets/images/icons/icon-192x192.png",
-// 	"/assets/images/icons/icon-384x384.png",
-// 	"/assets/images/icons/icon-512x512.png",
-// 	"/manifest.json",
-// 	"/register.js"
-// ];
+self.addEventListener('push', (event) => {
+	let body;
 
-// self.addEventListener("install", event => {
-// 	event.waitUntil(
-// 		caches.open(CACHE_NAME).then(cache => {
-// 			return cache.addAll(urlsToCache);
-// 		})
-// 	);
-// });
-
-// self.addEventListener("fetch", (event) => {
-// 	let base_url	= "https://api.football-data.org/v2/";
-
-// 	if (event.request.url.indexOf(base_url) > -1) {
-// 		event.respondWith(
-// 			caches.open(CACHE_NAME).then((cache) => {
-// 				return fetch(event.request).then((response) => {
-// 					cache.put(event.request.url, response.clone());
-					
-// 					return response;
-// 				})
-// 			})
-// 		);
-// 	} else {
-// 		event.respondWith(
-// 			caches.match(event.request, { ignoreSearch: true }).then((response) => {
-// 				return response || fetch (event.request);
-// 			})
-// 		)
-// 	}
-// });
-
-// self.addEventListener("activate", (event) => {
-// 	event.waitUntil(
-// 		caches.keys().then((cacheNames) => {
-// 			return Promise.all(
-// 				cacheNames.map((cacheName) => {
-// 					if (cacheName != CACHE_NAME) {
-// 						console.log("ServiceWorker: cache " + cacheName + " deleted.");
-
-// 						return caches.delete(cacheName);
-// 					}
-// 				})
-// 			);
-// 		})
-// 	);
-// });
-
-// self.addEventListener('push', (event) => {
-// 	let body;
-
-// 	if (event.data) {
-// 		body = event.data.text();
-// 	} else {
-// 		body = 'Push message no payload';
-// 	}
-// 	var options = {
-// 		body: body,
-// 		icon: 'assets/images/icon-72x72.png',
-// 		vibrate: [100, 50, 100],
-// 		data: {
-// 		dateOfArrival: Date.now(),
-// 			primaryKey: 1
-// 		}
-// 	};
-// 	event.waitUntil(
-// 	  self.registration.showNotification('Push Notification', options)
-// 	);
-// });
+	if (event.data) {
+		body = event.data.text();
+	} else {
+		body = 'Push message no payload';
+	}
+	let options = {
+		body: body,
+		icon: 'assets/images/icon-72x72.png',
+		vibrate: [100, 50, 100],
+		data: {
+		dateOfArrival: Date.now(),
+			primaryKey: 1
+		}
+	};
+	event.waitUntil(
+	  self.registration.showNotification('Push Notification', options)
+	);
+});
